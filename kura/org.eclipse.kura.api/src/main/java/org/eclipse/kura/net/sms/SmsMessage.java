d3f7995ab16fd5305206262cb84fa54b1fb3a77f
pierantoniomerlino <pierantonio.merlino@eurotech.com>
2016-04-05 10:56:47 +0200

    Added methods on sms service api
    Signed-off-by: pierantoniomerlino <pierantonio.merlino@eurotech.com>

--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/sms/SmsMessage.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/sms/SmsMessage.java
@@ -1,5 +1,45 @@
 package org.eclipse.kura.net.sms;
 
-public interface SmsMessage {
+public class SmsMessage {
 
+	private String content;  // content of the message
+	private String endpoint; // sender or receiver
+	private String type;     // inbound or outbound ENUM???
+
+	public SmsMessage() {
+		content = "";
+		endpoint = "";
+		type = "";
+	}
+	
+	public SmsMessage(String content, String endpoint, String type) {
+		this.content = content;
+		this.endpoint = endpoint;
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getEndpoint() {
+		return endpoint;
+	}
+
+	public void setEndpoint(String endpoint) {
+		this.endpoint = endpoint;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
--- kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/sms/SmsService.java
+++ kura/org.eclipse.kura.api/src/main/java/org/eclipse/kura/net/sms/SmsService.java
@@ -1,18 +1,15 @@
 package org.eclipse.kura.net.sms;
 
 import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 
 import org.eclipse.kura.KuraException;
-import org.eclipse.kura.net.modem.ModemDevice;
 import org.osgi.service.io.ConnectionFactory;
 
 public interface SmsService extends ConnectionFactory {
-	
-	public void addModem(ModemDevice modem) throws KuraException;
-	
-	public void removeModem(ModemDevice modem) throws KuraException;
-	
-	public SmsMessage[] readMessages() throws KuraException;
+
+	public List<SmsMessage> readMessages() throws KuraException;
 	
 	public boolean sendMessage(SmsMessage sms) throws KuraException;
 	
@@ -21,8 +18,31 @@ public interface SmsService extends ConnectionFactory {
 	public boolean queueMessage(SmsMessage sms) throws KuraException;
 	
 	public int queueMessages(Collection<SmsMessage> msgList) throws KuraException;
+
+	public boolean queueMessageAt(SmsMessage sms, Date at) throws KuraException;
 	
-	// Aggiungere altri metodi.
+	public boolean queueMessageAt(SmsMessage sms, long after) throws KuraException;
 	
+	public boolean removeMessageFromQueue(SmsMessage sms)  throws KuraException;
+	
+	public boolean deleteMessage(SmsMessage sms)  throws KuraException;
+	
+	public boolean sendUSSDCommand(String request) throws KuraException;
+	
+	public int getInboundMessageCount() throws KuraException;
+	
+	public int getOutboundMessageCount() throws KuraException;
+	
+	public void setInboundSmsMessageNotification(SmsMessageNotification notification) throws KuraException;
+	
+	public SmsMessageNotification getInboundSmsMessageNotification() throws KuraException;
+	
+	public void setOutboundSmsMessageNotification(SmsMessageNotification notification) throws KuraException;
+	
+	public SmsMessageNotification getOutboundSmsMessageNotification() throws KuraException;
+	
+	public void setUSSDNotification(USSDSmsNotification notification) throws KuraException;
+	
+	public USSDSmsNotification getUSSDNotification() throws KuraException;
 	
 }
